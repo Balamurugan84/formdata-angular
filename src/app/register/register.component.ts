4674de08a8588c6d6e12a7cb8b07d1178b849fce
Balamurugan84 <102845708+Balamurugan84@users.noreply.github.com>
2022-09-01 00:21:10 -0700

    Add files via upload

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { TmplAstNode } from '@angular/compiler';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+// export class RegisterComponent implements OnInit {
+export class RegisterComponent{
+
+  // constructor() { }
+
+  // ngOnInit(): void {
+  // }
+
+@ViewChild('userForm') uf:NgForm;
+
+  onSubmit=(userForm:NgForm)=>{
+    console.log(userForm)
+    console.log(userForm.value)
+  }
+
+
+  state = [{
+    state:'TN',
+  },
+{
+  state:'AP'
+},
+{
+  state:'KL'
+}]
+}
